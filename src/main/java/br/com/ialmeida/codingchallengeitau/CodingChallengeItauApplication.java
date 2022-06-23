ab0ac7ecda07dc002cbe6a64b2a8178e1e011460
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-22 22:14:54 -0300

    Project created

---
+++ src/main/java/br/com/ialmeida/codingchallengeitau/CodingChallengeItauApplication.java
@@ -0,0 +1,13 @@
+package br.com.ialmeida.codingchallengeitau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallengeItauApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingChallengeItauApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ialmeida/codingchallengeitau/CodingChallengeItauApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ialmeida.codingchallengeitau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingChallengeItauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
