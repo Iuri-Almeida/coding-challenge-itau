8a470464b1b9e28c1543300fdfc644be2ceb0321
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-23 06:20:51 -0300

    feat(film): OMDb API connection

---
+++ src/main/java/br/com/ialmeida/codingchallengeitau/clients/FilmClient.java
@@ -0,0 +1,13 @@
+package br.com.ialmeida.codingchallengeitau.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(url = "https://www.omdbapi.com/?apikey=51dc6e44", name = "films")
+public interface FilmClient {
+
+    @GetMapping
+    Object findFilmByName(@RequestParam(value = "t") String name);
+
+}
--- src/main/java/br/com/ialmeida/codingchallengeitau/controllers/FilmController.java
+++ src/main/java/br/com/ialmeida/codingchallengeitau/controllers/FilmController.java
@@ -1,31 +1,35 @@
 package br.com.ialmeida.codingchallengeitau.controllers;
 
+import br.com.ialmeida.codingchallengeitau.clients.FilmClient;
 import br.com.ialmeida.codingchallengeitau.entities.Film;
 import br.com.ialmeida.codingchallengeitau.services.FilmService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/films")
+@RequestMapping(value = "/api")
 @AllArgsConstructor
 public class FilmController {
 
     private final FilmService filmService;
+    private final FilmClient filmClient;
 
-    @GetMapping
+    @GetMapping(value = "/films")
     public ResponseEntity<List<Film>> findAll() {
         return ResponseEntity.ok().body(filmService.findAll());
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/films/{id}")
     public ResponseEntity<Film> findById(@PathVariable Long id) {
         return ResponseEntity.ok().body(filmService.findById(id));
+    }
+
+    @GetMapping(value = "/film")
+    public ResponseEntity<Object> findByName(@RequestParam(value = "name") String name) {
+        return ResponseEntity.ok().body(filmClient.findFilmByName(name));
     }
 
 }
