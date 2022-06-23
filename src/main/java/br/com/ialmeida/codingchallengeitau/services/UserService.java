3175d9ff4150dfb904c42dfd2533135678e465bb
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-23 05:46:22 -0300

    feat(user): add user service

---
+++ src/main/java/br/com/ialmeida/codingchallengeitau/services/UserService.java
@@ -0,0 +1,24 @@
+package br.com.ialmeida.codingchallengeitau.services;
+
+import br.com.ialmeida.codingchallengeitau.entities.User;
+import br.com.ialmeida.codingchallengeitau.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User with id = '" + id + "' not found."));
+    }
+
+}
