3bad7161288f413cf8b32ca912311e1e1f89cfa1
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-23 06:47:29 -0300

    refactor(film): film client method and param name

--- src/main/java/br/com/ialmeida/codingchallengeitau/clients/FilmClient.java
+++ src/main/java/br/com/ialmeida/codingchallengeitau/clients/FilmClient.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface FilmClient {
 
     @GetMapping
-    Object findFilmByName(@RequestParam(value = "t") String name);
+    Object findFilmByTitle(@RequestParam(value = "t") String title);
 
 }
