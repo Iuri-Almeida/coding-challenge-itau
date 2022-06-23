878ba88393ac967aab0323ae13931b88b040ea34
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-23 05:46:39 -0300

    feat(user): add user controller

---
+++ src/main/java/br/com/ialmeida/codingchallengeitau/controllers/UserController.java
@@ -0,0 +1,31 @@
+package br.com.ialmeida.codingchallengeitau.controllers;
+
+import br.com.ialmeida.codingchallengeitau.entities.User;
+import br.com.ialmeida.codingchallengeitau.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(userService.findById(id));
+    }
+
+}
