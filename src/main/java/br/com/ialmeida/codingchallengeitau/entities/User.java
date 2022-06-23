067a4b7fcc6ed2c110ad88deb348744b44f372ee
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-23 15:05:47 -0300

    feat(profile): add profile enum

--- src/main/java/br/com/ialmeida/codingchallengeitau/entities/User.java
+++ src/main/java/br/com/ialmeida/codingchallengeitau/entities/User.java
@@ -1,5 +1,6 @@
 package br.com.ialmeida.codingchallengeitau.entities;
 
+import br.com.ialmeida.codingchallengeitau.entities.enums.Profile;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,6 +36,8 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Rating> ratings;
 
+    private Profile profile;
+
     public User(Long id, String name, String email, String password) {
         this.id = id;
         this.name = name;
@@ -43,10 +46,19 @@ public class User {
         this.score = 0;
         this.comments = new ArrayList<>();
         this.ratings = new ArrayList<>();
+        this.profile = Profile.READER;
     }
 
     public void addScore() {
         score++;
+
+        if (score >= 20 && score < 100) {
+            this.setProfile(Profile.BASIC);
+        } else if (score >= 100 && score < 1000) {
+            this.setProfile(Profile.ADVANCED);
+        } else if (score >= 1000) {
+            this.setProfile(Profile.MODERATOR);
+        }
     }
 
 }
---
+++ src/main/java/br/com/ialmeida/codingchallengeitau/entities/enums/Profile.java
@@ -0,0 +1,10 @@
+package br.com.ialmeida.codingchallengeitau.entities.enums;
+
+public enum Profile {
+
+    READER,
+    BASIC,
+    ADVANCED,
+    MODERATOR
+
+}
