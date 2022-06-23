d8414c65287acdedafe00ee5866bd5c7e73b965c
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-22 22:26:44 -0300

    feat(user): add user entity

---
+++ src/main/java/br/com/ialmeida/codingchallengeitau/entities/User.java
@@ -0,0 +1,27 @@
+package br.com.ialmeida.codingchallengeitau.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private Integer score;
+
+}
