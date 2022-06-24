072a3019c576ab48483a0205132fb79f6358f7cc
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-24 08:57:54 -0300

    refactor(film): default film rating

--- src/main/java/br/com/ialmeida/codingchallengeitau/entities/Film.java
+++ src/main/java/br/com/ialmeida/codingchallengeitau/entities/Film.java
@@ -60,7 +60,7 @@ public class Film {
             sumRatings += r.getScore();
         }
 
-        return (totalRatings == 0) ? 0.0 : Math.round((sumRatings / totalRatings) * 100.0) / 100.0;
+        return (totalRatings == 0) ? null : Math.round((sumRatings / totalRatings) * 100.0) / 100.0;
     }
 
 }
