33d2668fed444a31d7d6db94182725ed852c5f2c
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-25 14:40:06 -0300

    feat(security): spring security configuration

---
+++ src/main/java/br/com/ialmeida/codingchallengeitau/security/config/JwtConfig.java
@@ -0,0 +1,49 @@
+package br.com.ialmeida.codingchallengeitau.security.config;
+
+import br.com.ialmeida.codingchallengeitau.details.services.impl.UserDetailServiceImpl;
+import br.com.ialmeida.codingchallengeitau.security.filter.authentication.JwtAuthenticationFilter;
+import br.com.ialmeida.codingchallengeitau.security.filter.validation.JwtValidationFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+@AllArgsConstructor
+public class JwtConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailServiceImpl userDetailService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtValidationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+}
