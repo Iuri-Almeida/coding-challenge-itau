4de77f3d93c3dbb231d34c47170eaaa082e70854
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-25 17:30:46 -0300

    feat(authentication): only moderators can insert new users

--- src/main/java/br/com/ialmeida/codingchallengeitau/services/UserService.java
+++ src/main/java/br/com/ialmeida/codingchallengeitau/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.ialmeida.codingchallengeitau.services;
 
 import br.com.ialmeida.codingchallengeitau.entities.User;
+import br.com.ialmeida.codingchallengeitau.entities.enums.Profile;
 import br.com.ialmeida.codingchallengeitau.exceptions.DuplicatedActionException;
 import br.com.ialmeida.codingchallengeitau.exceptions.NullParameterException;
 import br.com.ialmeida.codingchallengeitau.exceptions.ResourceNotFoundException;
@@ -34,6 +35,7 @@ public class UserService {
     public User insert(User user) {
         this.validateParams(user);
         this.validateUser(user);
+        this.validateUserProfile(user);
 
         user.setPassword(encoder.encode(user.getPassword()));
 
@@ -50,6 +52,12 @@ public class UserService {
         Optional<User> dbUser = userRepository.findByEmail(user.getEmail());
         if (dbUser.isPresent()) {
             throw new DuplicatedActionException("There is already a user with e-mail = '" + user.getEmail() + "'.");
+        }
+    }
+
+    private void validateUserProfile(User user) {
+        if (user.getProfile() != Profile.MODERATOR) {
+            throw new RuntimeException("Only moderators can insert users.");
         }
     }
 
