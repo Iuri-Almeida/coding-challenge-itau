48528a7d78e77b0e6754b8c789069ad66523400f
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-25 13:57:00 -0300

    feat(security): user detail service

---
+++ src/main/java/br/com/ialmeida/codingchallengeitau/details/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,29 @@
+package br.com.ialmeida.codingchallengeitau.details.service.impl;
+
+import br.com.ialmeida.codingchallengeitau.details.UserDetail;
+import br.com.ialmeida.codingchallengeitau.entities.User;
+import br.com.ialmeida.codingchallengeitau.exceptions.ResourceNotFoundException;
+import br.com.ialmeida.codingchallengeitau.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> dbUser = userRepository.findByEmail(email);
+        if (dbUser.isEmpty()) {
+            throw new ResourceNotFoundException("User with e-mail = '" + email + "' not found.");
+        }
+        return new UserDetail(dbUser.get());
+    }
+}
