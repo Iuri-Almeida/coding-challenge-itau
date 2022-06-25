17e9c82d5a431e9e5391a869dc032ae40dc3eac5
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-25 13:48:50 -0300

    feat(security): user detail

---
+++ src/main/java/br/com/ialmeida/codingchallengeitau/details/UserDetail.java
@@ -0,0 +1,50 @@
+package br.com.ialmeida.codingchallengeitau.details;
+
+import br.com.ialmeida.codingchallengeitau.entities.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+public class UserDetail implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return (user == null) ? (new User()).getPassword() : user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return (user == null) ? (new User()).getEmail() : user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
