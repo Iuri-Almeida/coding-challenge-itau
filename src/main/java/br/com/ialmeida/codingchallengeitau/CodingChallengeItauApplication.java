b1e11be0642212f882b3b8d83d3db768661831ce
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-06-28 19:02:37 -0300

    refactor(security): include security auto configuration

--- src/main/java/br/com/ialmeida/codingchallengeitau/CodingChallengeItauApplication.java
+++ src/main/java/br/com/ialmeida/codingchallengeitau/CodingChallengeItauApplication.java
@@ -2,13 +2,12 @@ package br.com.ialmeida.codingchallengeitau;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 @EnableFeignClients
 public class CodingChallengeItauApplication {
 
